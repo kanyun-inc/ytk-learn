e06affea44d754e462faef60e04018d8567a5098
xialong <glscharoun@gmail.com>
2017-05-19 16:32:23 +0800

    ADD:win_predict.bat for windows

---
+++ bin/win_predict.bat
@@ -0,0 +1,28 @@
+echo on
+@Rem model name(linear, fm, ffm, gbdt, gbmlr, gbsdt, gbhmlr, gbhsdt, multiclass_linear)
+set model_name=???
+
+set config_path=config/model/%model_name%.conf
+
+@Rem data file for predicting
+set file_name=???
+
+@Rem train/test line python transform switch & script
+set transform="false"
+set transform_script_path=bin/transform.py
+
+@Rem result save mode: PREDICT_RESULT_ONLY, LABEL_AND_PREDICT, PREDICT_AS_FEATURE
+set resultSaveMode=PREDICT_RESULT_ONLY
+set resultFileSuffix=_%model_name%_%resultSaveMode%
+
+@Rem max error data format tolerate number
+set max_error_tol=100
+
+set eval_metric="auc,mae"
+
+@Rem value or leafid
+set predict_type=value
+
+@start /b cmd /c java -Xmx1000m -XX:-OmitStackTraceInFastThrow -cp lib/* -Dlog4j.configuration=file:config/log4j.properties com.fenbi.ytklearn.predictor.Predicts %config_path% %model_name% %file_name% %transform% %transform_script_path% %resultSaveMode% %resultFileSuffix% %max_error_tol% %eval_metric% %predict_type%
+
+pause
